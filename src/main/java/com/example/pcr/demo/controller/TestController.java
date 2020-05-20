3f6ea2a4c7d5adadac535885ad116efa6ce27d46
chengri.p <chengri.piao>
2020-05-20 16:41:53 +0800

    s

---
+++ src/main/java/com/example/pcr/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.pcr.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chengri.piao
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/5/18 13:49
+ */
+@RestController
+public class TestController {
+
+}
